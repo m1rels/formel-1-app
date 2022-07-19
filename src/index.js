f2edd9661dc08d4742bec3767cda15a54c89b65d
Mirel Korajac <mirelkorajac@gmail.com>
2022-07-19 22:05:20 +0200

    Route Config

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import App from './App';
+import DriversTable from './components/DriversTable';
+import ConstructorsTable from "./components/ConstructorsTable";
 import './index.css';
 
 const root = ReactDOM.createRoot(
@@ -8,5 +11,12 @@ const root = ReactDOM.createRoot(
 );
 
 root.render(
-  <App />
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route path="/drivers" element={<DriversTable />}/>
+        <Route path="/constructors" element={<ConstructorsTable />}/>
+      </Route>
+    </Routes>
+  </BrowserRouter>
 );
