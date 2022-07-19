bc4db4192c1fc6b68c8a9b8032ebdad45b78dfeb
Mirel Korajac <mirelkorajac@gmail.com>
2022-07-19 22:05:48 +0200

    Add Links to App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
-import DriversTable from "./components/DriversTable";
-import Menu from "./components/Menu";
+import {Link, Outlet} from "react-router-dom";
+import "./styles.css"
 import './App.css';
 
 export default function App() {
   return (
     <div>
       <h1>F1</h1>
-      <Menu />
-      <DriversTable />
 
+      <nav>
+        <ul className="tab tab-block ">
+          <li className="tab-item text-dark active">
+            <Link to="/drivers" className='MenuLink'>Drivers</Link>
+          </li>
+           <li className="tab-item text-dark">
+            <Link to="/constructors" className='MenuLink'>Constructors</Link>
+           </li>
+        </ul>
+      </nav>
+      <Outlet />
     </div>
   )
 }
