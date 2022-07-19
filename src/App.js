71a6ce0d203f36f2c3c1556babde01de4b9bae1f
Mirel Korajac <mirelkorajac@gmail.com>
2022-07-19 18:27:46 +0200

    Render in the DOM

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import DriversTable from "./components/DriversTable";
+import Menu from "./components/Menu";
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+export default function App() {
+  return (
+    <div>
+      <h1>F1</h1>
+      <Menu />
+      <DriversTable />
+
+    </div>
+  )
 }
 
-export default App;
