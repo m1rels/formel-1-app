ccd0960e66e1a11f7dc8dac3dd60c6bb10c16afb
Mirel Korajac <mirelkorajac@gmail.com>
2022-07-19 22:04:02 +0200

    Add style to ConstructorsTable

--- src/components/ConstructorsTable.js
+++ src/components/ConstructorsTable.js
@@ -5,20 +5,20 @@ export default function DriversTable() {
     <table className="table">
   <thead>
     <tr>
-      <th className="text-normal">Rank</th>
-      <th className="text-normal">Driver</th>
-      <th className="text-normal">Points</th>
-      <th className="text-normal">Wins</th>
-      <th className="text-normal">Podiums</th>
+      <th className="text-normal column col-1 text-center">Rank</th>
+      <th className="text-normal column col-5">Driver</th>
+      <th className="text-normal column col-2 text-center">Points</th>
+      <th className="text-normal column col-2 text-center">Wins</th>
+      <th className="text-normal column col-2 text-center">Podiums</th>
     </tr>
   </thead>
   <tbody>
     <tr>
-      <td><b>1</b></td>
+      <td className="text-center"><b>1</b></td>
       <td><b>Red Bull</b></td>
-      <td><b>304</b></td>
-      <td><b>7</b></td>
-      <td><b>12</b></td>
+      <td className="text-center"><b>304</b></td>
+      <td className="text-center"><b>7</b></td>
+      <td className="text-center"><b>12</b></td>
     </tr>
   </tbody>
 </table>
