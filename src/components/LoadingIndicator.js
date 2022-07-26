32476e27ee2264bcba22358a147c6ec2e4bad363
Mirel Korajac <mirelkorajac@gmail.com>
2022-07-26 21:49:48 +0200

    Add banner while data-loading

---
+++ src/components/LoadingIndicator.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function LoadingIndicator() {
+    return(
+        <div>
+            <h1>Drivers/constructors are loading...</h1>
+        </div>
+    );
+}
