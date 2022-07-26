b34157dffc87398cbca4b9f7405a2f7da74f55b5
Mirel Korajac <mirelkorajac@gmail.com>
2022-07-26 21:50:09 +0200

    Adjust index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import App from './App';
-import DriversTable from './components/DriversTable';
-import ConstructorsTable from "./components/ConstructorsTable";
+import DriversPage from "./components/DriversPage";
+import ConstructorsPage from './components/ConstructorsPage';
 import './index.css';
 
 const root = ReactDOM.createRoot(
@@ -14,8 +14,8 @@ root.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />}>
-        <Route path="/drivers" element={<DriversTable />}/>
-        <Route path="/constructors" element={<ConstructorsTable />}/>
+        <Route path="/drivers" element={<DriversPage />}/>
+        <Route path="/constructors" element={<ConstructorsPage />}/>
       </Route>
     </Routes>
   </BrowserRouter>
