93fd3b6a681843caabf71ff345932997440841e9
Mirel Korajac <82174633+m1rels@users.noreply.github.com>
2022-07-31 19:05:50 +0200

    Config Routes

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import "./index.css";
 import Drivers from "./components/Drivers";
 import Constructors from "./components/Constructors";
 import SeasonList from "./components/SeasonList";
+import Driver from "./components/Driver";
+import Constructor from "./components/Constructor";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -13,9 +15,11 @@ root.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />}>
-        <Route path="/seasons" element={<SeasonList />}></Route>
-        <Route path="/seasons/:year/drivers" element={<Drivers />}></Route>
-        <Route path="/seasons/:year/constructors" element={<Constructors />}></Route>
+        <Route path="/seasons" element={<SeasonList />} />
+        <Route path="/seasons/:year/drivers" element={<Drivers />} />
+        <Route path="/seasons/:year/constructors" element={<Constructors />} />
+        <Route path="/drivers/:driverId" element={<Driver />} />
+        <Route path="/constructors/:constructorId" element={<Constructor />} />
       </Route>
     </Routes>
   </BrowserRouter>
