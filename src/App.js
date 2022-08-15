565c12a04f26bdb8d54a37dc38451712fbb80b98
Mirel Korajac <82174633+m1rels@users.noreply.github.com>
2022-08-15 10:50:11 +0200

    Implement SeasonList.js in App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
 import React from "react";
-import { Outlet, Link } from "react-router-dom";
 import "./styles.css";
 import "./App.css";
+import LoadingIndicator from "./components/LoadingIndicator";
+import { Link } from "react-router-dom";
 
 export default function App() {
-  return (
-    <React.Fragment>
-      <header>
-        <h1>F1</h1>
-        <ul className="nav">
-          <li className="nav-item active">
-            <Link to="/seasons">
-              Seasons
-            </Link>
-          </li>
-        </ul>
-      </header>
-      <Outlet />
-    </React.Fragment>
-  );
+  const [allSeasons, setAllSeasons] = React.useState([]);
+
+  React.useEffect(() => {
+    async function loadData () {
+        const response = await fetch(`http://ergast.com/api/f1/seasons.json?limit=100`, {});
+        const seasons = await response.json();
+        setAllSeasons(seasons.MRData.SeasonTable.Seasons);
+    }
+    loadData();
+  }, []);
+
+  if (!allSeasons) {
+    return <LoadingIndicator />;
+  }
+
+
+  const seasons = [];
+  allSeasons.forEach((season) => {
+    seasons.push(
+    <React.Fragment key={season.season}>
+      <h3 className="m-1 mt-2">{season.season}</h3>
+      <li>
+        <Link to={`/seasons/${season.season}/drivers`}>Drivers</Link>
+      </li>
+      <li>
+        <Link to={`/seasons/${season.season}/constructors`}>Constructors</Link>
+      </li>
+      <li>
+        <Link to={`/seasons/${season.season}/races`}>Race Schedule</Link>
+      </li>
+      </React.Fragment>
+    );
+  });
+
+  return seasons;
 }
